58f4a94bc893d41f8cad1b606dedcd045881efaf
Dmitrii Glukhov <dmitriy.a.glukhov@gmail.com>
2025-07-03 01:59:41 +0300

    Add neovim

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set expandtab")
+vim.cmd("set smartindent")
+vim.cmd("set number")
+vim.wo.relativenumber = true
+vim.g.mapleader = " "
+
